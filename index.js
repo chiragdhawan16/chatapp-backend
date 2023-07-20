b88f310771e13b23139e63f022d9ed6d8608a529
ChiragDhawan <chiragdhawan16@gmail.com>
2023-07-21 00:15:20 +0530

    first commit

--- index.js
+++ index.js
@@ -36,70 +36,70 @@ const eserver =app.listen(PORT, () => {
     console.log('Listening on PORT :' + PORT);
 });
 
-// const io=new Server(eserver,{
+const io=new Server(eserver,{
    
-//     cors: {
-//       // origin: "https://delightful-mousse-0f683c.netlify.app",
-//       // credentials: true,
-//     },
-// });
+    cors: {
+       origin: "https://timely-bonbon-e413fb.netlify.app",
+      // credentials: true,
+    },
+});
 
-// let users=[];
-// io.on("connection",(socket)=>{
+let users=[];
+io.on("connection",(socket)=>{
    
 
-//        socket.on("add user", (userid) => {
-//         console.log("user Connetcted-"+userid)  
-//         if(users.length===0)
-//          {   
+       socket.on("add user", (userid) => {
+        console.log("user Connetcted-"+userid)  
+        if(users.length===0)
+         {   
             
-//             users.push({userid:userid,socketid:socket.id})
+            users.push({userid:userid,socketid:socket.id})
             
-//         }
-//         else{
-//            if(!users.find(user=>user.userid===userid)) {
-//             users.push({userid:userid,socketid:socket.id})
-//            }
+        }
+        else{
+           if(!users.find(user=>user.userid===userid)) {
+            users.push({userid:userid,socketid:socket.id})
+           }
             
-//         }
+        }
         
-//       });
+      });
 
-//       socket.on("new message", async(newMessageRecieved,senderChat,senderid) => {
-//         console.log("message send by-" +senderid)
-//         if(users.length===0) return
-//           let chatUsers =await findUSerandChat(senderChat.chat._id)
+      socket.on("new message", async(newMessageRecieved,senderChat,senderid) => {
+        console.log("message send by-" +senderid)
+        if(users.length===0) return
+          let chatUsers =await findUSerandChat(senderChat.chat._id)
 
-//           chatUsers.forEach((user) => {
+          chatUsers.forEach((user) => {
            
-//             if (user ===senderid ) return;
+            if (user ===senderid ) return;
 
-//               let socket_id=users.find(user1=>user1.userid===user)?.socketid
-//               console.log("message sent to-"+socket_id)
-//               socket.to(socket_id).emit("message recieved", senderChat.chat._id);
-//           //  console.log(user.socketid)
-//           //  socket.to(user.socketid).emit("message recieved", user);
-//           // if(users.find(user1=>user1.userid===user)?.socketid)
-//           // {
-//           //   let socket_id=users.find(user1=>user1.userid===user)?.socketid
-//           //   socket.to(socket_id).emit("message recieved", user);
-//           // }
+              let socket_id=users.find(user1=>user1.userid===user)?.socketid
+              console.log("message sent to-"+socket_id)
+              socket.to(socket_id).emit("message recieved", senderChat.chat._id);
+          //  console.log(user.socketid)
+          //  socket.to(user.socketid).emit("message recieved", user);
+          // if(users.find(user1=>user1.userid===user)?.socketid)
+          // {
+          //   let socket_id=users.find(user1=>user1.userid===user)?.socketid
+          //   socket.to(socket_id).emit("message recieved", user);
+          // }
 
-//           })
-//         //   socket.to(senderChat.chat._id).emit("message recieved", senderChat);
+          })
+        //   socket.to(senderChat.chat._id).emit("message recieved", senderChat);
               
-//       });
+      });
 
-// })
+})
 
 
-//   const findUSerandChat=async(chatid)=>{
+  const findUSerandChat=async(chatid)=>{
     
-//     const chat = await Chat.findOne({ _id :chatid })
-//     return (chat.users.toString().split(","))
+    const chat = await Chat.findOne({ _id :chatid })
+    return (chat.users.toString().split(","))
 
 
-//   }
+  }
   
 
 
